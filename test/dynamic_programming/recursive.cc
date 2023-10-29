01b50c4e3504b23862dc43245ac6d61da9818e6b
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-10-29 12:50:47 +0000

    Feat: Implement Knapsack
    - Implement knapsack with recursive and memoized recursive algorithms.
    - Add tests for the two different methods but the number of tests in
      this commit are unsatisfactory.

---
+++ test/dynamic_programming/recursive.cc
@@ -0,0 +1,38 @@
+#ifndef CAW_TEST_DYNAMIC_PROGRAMMING_RECURSIVE_CC_
+#define CAW_TEST_DYNAMIC_PROGRAMMING_RECURSIVE_CC_
+
+#include <gtest/gtest.h>
+
+#include <utility>
+
+#include "dynamic_programming/knapsack.h"
+
+using TestCase = std::pair<VecItems, float>;
+
+class KnapsackRecursiveTf : public ::testing::TestWithParam<TestCase>
+{
+protected:
+  TestCase param;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+  Knapsack, KnapsackRecursiveTf,
+  ::testing::Values(TestCase(
+    VecItems{
+      Item("a", 3, 5.0), Item("b", 4, 6.0), Item("c", 5, 3.0), Item("d", 2, 1.0)
+    },
+    7.0
+  ))
+);
+
+TEST_P(KnapsackRecursiveTf, Works)
+{
+  const TestCase testCase = GetParam();
+  Knapsack *k = new Knapsack(testCase.first);
+  auto response = k->recursive(7.0);
+  uint expected = testCase.second;
+
+  ASSERT_EQ(response, expected);
+}
+
+#endif /* CAW_TEST_DYNAMIC_PROGRAMMING_RECURSIVE_CC_ */

2f76a6569b01a4bdfe16eaf3c22dc835582d6549
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-10-29 12:50:47 +0000

    Feat: Implement Knapsack
    - Implement knapsack with recursive and memoized recursive algorithms.
    - Add tests for the two different methods but the number of tests in
      this commit are unsatisfactory.

---
+++ test/dynamic_programming/knapsack_unit_test.cc
@@ -0,0 +1,9 @@
+#ifndef CAW_TEST_DYNAMIC_PROGRAMMING_KNAPSACK_UNIT_TEST_CC_
+#define CAW_TEST_DYNAMIC_PROGRAMMING_KNAPSACK_UNIT_TEST_CC_
+
+#include <gtest/gtest.h>
+
+#include "./dynamic.cc"
+#include "./recursive.cc"
+
+#endif /* CAW_TEST_DYNAMIC_PROGRAMMING_KNAPSACK_UNIT_TEST_CC_ */
--- test/main.cc
+++ test/main.cc
@@ -3,17 +3,17 @@
 
 #include <gtest/gtest.h>
 
-#include "sorting_algorithms/counting_sort_unit_test.cc"
-#include "sorting_algorithms/insertion_sort_unit_test.cc"
-#include "sorting_algorithms/fisher_yates_unit_test.cc"
-#include "sorting_algorithms/max_heap_unit_test.cc"
-#include "sorting_algorithms/max_heap_sort_unit_test.cc"
-#include "sorting_algorithms/min_heap_unit_test.cc"
-#include "sorting_algorithms/min_heap_sort_unit_test.cc"
-#include "sorting_algorithms/bubble_sort_unit_test.cc"
-#include "sorting_algorithms/quick_sort_unit_test.cc"
-#include "sorting_algorithms/merge_sort_unit_test.cc"
-
 #include "array/array_unit_test.cc"
+#include "dynamic_programming/knapsack_unit_test.cc"
+#include "sorting_algorithms/bubble_sort_unit_test.cc"
+#include "sorting_algorithms/counting_sort_unit_test.cc"
+#include "sorting_algorithms/fisher_yates_unit_test.cc"
+#include "sorting_algorithms/insertion_sort_unit_test.cc"
+#include "sorting_algorithms/max_heap_sort_unit_test.cc"
+#include "sorting_algorithms/max_heap_unit_test.cc"
+#include "sorting_algorithms/merge_sort_unit_test.cc"
+#include "sorting_algorithms/min_heap_sort_unit_test.cc"
+#include "sorting_algorithms/min_heap_unit_test.cc"
+#include "sorting_algorithms/quick_sort_unit_test.cc"
 
 #endif /* CAW_TEST_MAIN_CC_ */
